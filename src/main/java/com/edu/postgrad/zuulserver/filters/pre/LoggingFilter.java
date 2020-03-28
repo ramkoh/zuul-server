f228b1306bbb82a54a685a4ed136984c39ec6f68
ramkoh <raman.a.kohli@gmail.com>
2020-03-28 18:53:15 +0000

    Zuul server with a pre-filter

---
+++ src/main/java/com/edu/postgrad/zuulserver/filters/pre/LoggingFilter.java
@@ -0,0 +1,36 @@
+package com.edu.postgrad.zuulserver.filters.pre;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class LoggingFilter extends ZuulFilter {
+    Logger log = LoggerFactory.getLogger(LoggerFactory.class);
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        log.info("Request method {} and URL {}", request.getMethod(), request.getRequestURL());
+        return null;
+    }
+}
